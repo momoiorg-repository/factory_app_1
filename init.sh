bb633eb2740ad40a4928905ebcb3b7da8004c113
Kazuya Tago <ktago.gm@gmail.com>
2025-11-26 22:47:14 +0900

    add run_world command

--- init.sh
+++ init.sh
@@ -8,4 +8,5 @@ git clone https://github.com/isaac-sim/IsaacSim-ros_workspaces.git
 ./finstall.sh https://github.com/momoiorg-repository/melon_ros2.git
 ./finstall.sh https://github.com/momoiorg-repository/factory_world1.git world
 ./run_isaac_sim_docker.sh
+docker cp $FACTORY_BASE/factory_app_1/fenv/run_world isaac-sim-ws:/usr/local/bin
 ./connect_to_container.sh
