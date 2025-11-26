2e0cf133bf174cbcc5a72ac322013ec540f351df
Kazuya Tago <ktago.gm@gmail.com>
2025-11-26 18:59:16 +0900

    update init.sh

---
+++ init.sh
@@ -0,0 +1,10 @@
+export FACTORY_BASE=`pwd`
+echo "export FACTORY_BASE=`pwd`" >> ~/.bashrc
+git clone https://github.com/momoiorg-repository/at_factory.git
+cd at_factory
+git clone https://github.com/isaac-sim/IsaacSim-ros_workspaces.git
+./init.sh
+./finstall.sh https://github.com/momoiorg-repository/melon_ros2.git
+./finstall.sh https://github.com/momoiorg-repository/factory_world1.git world
+./run_isaac_sim_docker.sh
+./connect_to_container.sh
