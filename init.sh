68c416d5aa5192a1f87f30c694ede9dfe17eb918
Kazuya Tago <ktago.gm@gmail.com>
2025-12-02 16:29:46 +0900

    update init.sh

--- init.sh
+++ init.sh
@@ -20,6 +20,8 @@ git clone https://github.com/isaac-sim/IsaacSim-ros_workspaces.git
 ./init.sh
 ./finstall.sh https://github.com/momoiorg-repository/melon_ros2.git
 ./finstall.sh https://github.com/momoiorg-repository/factory_world1.git world
-./run_isaac_sim_docker.sh
-docker cp $FACTORY_BASE/factory_app_1/fenv/run_world isaac-sim-ws:/usr/local/bin/run_world
-./connect_to_container.sh
+cd ..
+source ~/.bashrc
+fct run
+docker cp ./factory_app_1/fenv/run_world isaac-sim-ws:/usr/local/bin/run_world
+fct con
