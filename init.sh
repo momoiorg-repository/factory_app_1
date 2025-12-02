b3eae8900f49581378c52bdf8a956fdcaa645b69
Kazuya Tago <ktago.gm@gmail.com>
2025-12-02 16:10:00 +0900

    update fct and act

--- init.sh
+++ init.sh
@@ -1,3 +1,16 @@
+mkdir config
+
+while (( $# > 0))
+do
+  case $1 in
+    '--cloud')
+      echo "CLOUD_IP=$2" >> ./config/.env
+      shift
+      ;;
+  esac
+  shift
+done
+
 export FACTORY_BASE=`pwd`
 echo "export FACTORY_BASE=`pwd`" >> ~/.bashrc
 echo "PATH=\$PATH:$FACTORY_BASE/factory_app_1/lbin" >> ~/.bashrc
@@ -8,5 +21,5 @@ git clone https://github.com/isaac-sim/IsaacSim-ros_workspaces.git
 ./finstall.sh https://github.com/momoiorg-repository/melon_ros2.git
 ./finstall.sh https://github.com/momoiorg-repository/factory_world1.git world
 ./run_isaac_sim_docker.sh
-docker cp $FACTORY_BASE/factory_app_1/fenv/run_world isaac-sim-ws:/usr/local/bin
+docker cp $FACTORY_BASE/factory_app_1/fenv/run_world isaac-sim-ws:/usr/local/bin/run_world
 ./connect_to_container.sh
